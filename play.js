2b9557f9e7838df3956b36e8633d455e7a421d33
Stephen Huang <stephen_1996@live.ca>
2022-03-17 03:02:15 +0000

    added ctrl+c quit

--- play.js
+++ play.js
@@ -15,3 +15,21 @@ const connect = () => {
 
 console.log("Connecting ...");
 connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", function (key) {
+    const handleUserInput = function () {
+      if (data === '\u0003') {
+        process.exit();
+      } 
+    };
+  });
+  return stdin;
+};
+
+module.export = setupInput;
