d6433efc0a12c78c822dbdc34d46d97a42830cf4
Stephen Huang <stephen_1996@live.ca>
2022-03-17 02:29:03 +0000

    added connect message and name

--- client.js
+++ client.js
@@ -4,11 +4,16 @@ const net = require("net");
 const connect = () => {
   const conn = net.createConnection({
     host: '165.227.47.243',
-    port: 50541
+    port: 50541,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log('Successfully connected to game server')
+    conn.write('Name: SH');
+  })
 
   return conn;
 };
